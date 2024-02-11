89567994741f0bf4a1b37f58ed54480c129995f4
FrigusRex <57226765+PranavGovekar@users.noreply.github.com>
2024-02-11 18:00:21 +0530

    Update ramp.c

--- ramp.c
+++ ramp.c
@@ -1,20 +1,24 @@
-// #define RAMP_DOWN
+#define RAMP_DOWN
+
+#define RESOLUTION 12
+#define MAX_LEVEL ((1<<RESOLUTION) - 1)
+
+#define NOP_DELAY 21
+
 
 void setup() {
-  analogWriteResolution(12);
-  // pinMode(DAC, OUTPUT);
+  analogWriteResolution(RESOLUTION);
 }
 
 void loop() {
-  for (int i=0; i<4095; i++){
+  for (int i=0; i<MAX_LEVEL; i++){
     #ifdef RAMP_DOWN  
-      analogWrite(DAC, 4095-i);
+      analogWrite(DAC, MAX_LEVEL-i);
     #else  
       analogWrite(DAC, i);
     #endif 
-
     // volatile int wasteTime = 0;
-    for (int t=0; t<23; t++){
+    for (int t=0; t<NOP_DELAY; t++){
       // wasteTime++;
         __asm__("nop\n\t");
     }
